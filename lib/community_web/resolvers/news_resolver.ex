9848dc6db80c2dc5ea31b77a147c1446ef458d6e
Jéssica Lins <jessicaalins@gmail.com>
2019-09-26 17:40:08 +0200

    Add resolver

---
+++ lib/community_web/resolvers/news_resolver.ex
@@ -0,0 +1,8 @@
+defmodule NewsResolver do
+    alias Community.News
+
+    def all_links(_root, _args, _info) do
+        links = News.list_links()
+        {:ok, links}
+    end
+end
--- lib/community_web/schema.ex
+++ lib/community_web/schema.ex
@@ -10,6 +10,10 @@ defmodule CommunityWeb.Schema do
   end
 
   query do
-    field :all_links, non_null(list_of(non_null(:link)))
+    field :all_links, non_null(list_of(non_null(:link))) do
+        # Reference to the 3 arity function all_links
+        # found in the CommunityWeb.NewsResolver module
+        resolve &NewsResolver.all_links/3
+    end
   end
 end
