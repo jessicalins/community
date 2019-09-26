3825273efc6990eb0e796b1ff45b51dfb85674a8
Jéssica Lins <jessicaalins@gmail.com>
2019-09-26 18:39:41 +0200

    Add create link mutation

--- lib/community_web/resolvers/news_resolver.ex
+++ lib/community_web/resolvers/news_resolver.ex
@@ -5,4 +5,14 @@ defmodule CommunityWeb.NewsResolver do
         links = News.list_links()
         {:ok, links}
     end
+
+    def create_link(_root, args, _info) do
+        # TODO: Add detailed error handling msg later
+        case News.create_link(args) do
+        {:ok, link} ->
+            {:ok, link}
+        _error ->
+            {:error, "Could not create link"}
+        end
+    end
 end
